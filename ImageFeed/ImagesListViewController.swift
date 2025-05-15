ad78ed267e7eedccc7df76f84e7a031981994862
soulhidee <serverpgig@yandex.ru>
2025-05-15 19:58:18 +0300

    feat: Set background color using UIBlack asset

---
+++ ImageFeed/ImagesListViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+final class ImagesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+
+    private func setupUI() {
+        view.backgroundColor = UIColor(named: "UIBlack")
+    }
+    
+
+}
