3b34dceff7945c0384ae62846b0afbb875a8d17b
soulhidee <serverpgig@yandex.ru>
2025-05-16 12:55:54 +0300

    feat: Create ImagesListCell class and organize files into ImagesList group

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -9,11 +9,11 @@ extension ImagesListViewController: UITableViewDelegate {
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+        return UITableViewCell()
     }
     
     
