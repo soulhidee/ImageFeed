8759996d061d32a29d2d1b377ec6eb95875feb74
soulhidee <serverpgig@yandex.ru>
2025-05-16 12:45:14 +0300

    feat: Add UITableViewDelegate & UITableViewDataSource via extensions

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -1,16 +1,40 @@
 import UIKit
 
-final class ImagesListViewController: UIViewController {
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt idexPath: IndexPath) {
+        
+    }
+    
+}
 
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    
+}
+
+
+final class ImagesListViewController: UIViewController {
+ 
+    
+    @IBOutlet private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
 
     private func setupUI() {
         view.backgroundColor = UIColor(named: "UIBlack")
     }
-    
 
 }
