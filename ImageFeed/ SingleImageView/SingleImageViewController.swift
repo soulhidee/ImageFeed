8304cfbfbd9ca3db4f787609345688c92e4b5c3a
soulhidee <serverpgig@yandex.ru>
2025-05-24 15:05:52 +0300

    feat: Add back button setup and dismiss logic

--- ImageFeed/ SingleImageView/SingleImageViewController.swift
+++ ImageFeed/ SingleImageView/SingleImageViewController.swift
@@ -7,6 +7,8 @@ final class SingleImageViewController: UIViewController {
             imageView.image = image
         }
     }
+
+    @IBOutlet private weak var backButton: UIButton!
     
     @IBOutlet private weak var imageView: UIImageView!
     
@@ -14,5 +16,15 @@ final class SingleImageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        setupBackButton()
+    }
+
+    @IBAction private func didTapBackButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    private func setupBackButton() {
+        backButton.setTitle("", for: .normal)
+        backButton.setImage(UIImage(named: "Backward"), for: .normal)
     }
 }
