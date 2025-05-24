ef699a24576711d2a0e0f8f5655aa7289c4813bb
soulhidee <serverpgig@yandex.ru>
2025-05-24 15:07:05 +0300

    refactor: Clean up and organize SingleImageViewController

--- ImageFeed/ SingleImageView/SingleImageViewController.swift
+++ ImageFeed/ SingleImageView/SingleImageViewController.swift
@@ -1,28 +1,32 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController {
+    
+    // MARK: - Public Properties
     var image: UIImage? {
         didSet {
             guard isViewLoaded else { return }
             imageView.image = image
         }
     }
-
+    
+    // MARK: - Outlets
+    @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var backButton: UIButton!
     
-    @IBOutlet private weak var imageView: UIImageView!
-    
-    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
         setupBackButton()
     }
-
+    
+    // MARK: - Actions
     @IBAction private func didTapBackButton(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
     
+    // MARK: - Private Methods
     private func setupBackButton() {
         backButton.setTitle("", for: .normal)
         backButton.setImage(UIImage(named: "Backward"), for: .normal)
