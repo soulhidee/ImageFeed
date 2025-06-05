0f5e17534f9144e35cb8d68db0e86fbbc7eb9848
soulhidee <serverpgig@yandex.ru>
2025-06-05 17:22:38 +0300

    feat: Configure sign-in button and add layout constraints

--- ImageFeed/AuthViewController.swift
+++ ImageFeed/AuthViewController.swift
@@ -13,11 +13,18 @@ class AuthViewController: UIViewController {
     private func setupViews() {
         configureView()
         configureAuthLogoImageView()
+        configureSignInButton()
     }
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             authLogoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            authLogoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            authLogoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            signInButton.heightAnchor.constraint(equalToConstant: 48),
+            signInButton.topAnchor.constraint(equalTo: authLogoImageView.bottomAnchor, constant: 204),
+            signInButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            signInButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+            
         ])
     }
     
@@ -29,5 +36,15 @@ class AuthViewController: UIViewController {
         authLogoImageView.image = UIImage(named: "auth_screen_logo")
         authLogoImageView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(authLogoImageView)
+    }
+    
+    private func configureSignInButton() {
+        signInButton.setTitle("Войти", for: .normal)
+        signInButton.backgroundColor = UIColor(named: "YPWhite") ?? .white
+        signInButton.layer.cornerRadius = 16
+        signInButton.setTitleColor(UIColor(named: "YPBlack"), for: .normal)
+        signInButton.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(signInButton)
     }
 }
