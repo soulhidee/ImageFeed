bed6590aaf553c7980620d6fdfb1cc22d35f27a0
soulhidee <serverpgig@yandex.ru>
2025-06-05 16:23:04 +0300

    feat: Create AuthViewController with base setup methods

---
+++ ImageFeed/AuthViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class AuthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+    }
+    private func setupViews() {
+    }
+    private func setupConstraints() {
+        
+    }
+    
+
+}
