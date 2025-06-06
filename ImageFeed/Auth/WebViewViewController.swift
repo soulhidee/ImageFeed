45a92761e06b3d27449db66c9e382bb0cc015aef
soulhidee <serverpgig@yandex.ru>
2025-06-06 13:16:30 +0300

    feat: Add and style WebView with constraints

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -1,5 +1,40 @@
 import UIKit
+import WebKit
 
 final class WebViewViewController: UIViewController {
+    let webView = WKWebView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        configureWebView()
+        configureView()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: .zero),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: .zero),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: .zero),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: .zero)
+            
+            
+        ])
+    }
+    private func configureView() {
+        view.backgroundColor = UIColor(named: "YPWhite") ?? .white
+    }
+    
+    private func configureWebView() {
+        webView.backgroundColor = .white
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(webView)
+    }
+    
     
 }
