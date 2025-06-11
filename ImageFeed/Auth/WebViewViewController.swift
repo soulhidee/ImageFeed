cd3da1f12f2e007b2153b0f6b0bffffc3ee1c480
soulhidee <serverpgig@yandex.ru>
2025-06-11 13:54:58 +0300

    feat: Add OAuth token request builder

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -17,7 +17,7 @@ final class WebViewViewController: UIViewController {
         loadAuthView()
         setupConstraints()
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         webView.addObserver(
@@ -26,7 +26,7 @@ final class WebViewViewController: UIViewController {
             options: .new,
             context: nil)
     }
-
+    
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress))
@@ -108,6 +108,41 @@ final class WebViewViewController: UIViewController {
         webView.load(request)
     }
     
+    func makeOAuthTokenRequest(code: String) throws -> URLRequest {
+        guard !code.isEmpty else {
+            throw RequestError.invalidCode
+        }
+        
+        guard let url = URL(string: WebViewConstants.tokenURLString) else {
+            throw RequestError.invalidURL
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.timeoutInterval = 30.0
+        
+        let bodyParameters = [
+            "client_id": Constants.accessKey,
+            "client_secret": Constants.secretKey,
+            "redirect_uri": Constants.redirectURI,
+            "code": code,
+            "grant_type": "authorization_code"
+        ]
+        
+        var components = URLComponents()
+        components.queryItems = bodyParameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+        
+        guard let bodyString = components.query,
+              let bodyData = bodyString.data(using: .utf8) else {
+            throw RequestError.invalidBodyEncoding
+        }
+        
+        request.httpBody = bodyData
+        
+        return request
+    }
+    
     // MARK: - Helpers
     private func code(from navigationAction: WKNavigationAction) -> String? {
         if
@@ -135,6 +170,19 @@ final class WebViewViewController: UIViewController {
     // MARK: - Enum
     enum WebViewConstants {
         static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+        static let unsplashBaseURLString = "https://unsplash.com"
+        static let tokenPath = "/oauth/token"
+        
+        static var tokenURLString: String {
+            return unsplashBaseURLString + tokenPath
+        }
+    }
+    
+    enum RequestError: Error {
+        case invalidBaseURL
+        case invalidURL
+        case invalidCode
+        case invalidBodyEncoding
     }
 }
 
