822ad2e48b481063c278ecac8160ffc0d294fba9
soulhidee <serverpgig@yandex.ru>
2025-06-12 12:18:57 +0300

    feat: Add URLSession extension with custom data method

---
+++ ImageFeed/Auth/URLSession+data.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+enum NetworkError: Error {
+    case httpStatusCode(Int)
+    case urlRequestError(Error)
+    case urlSessionError
+}
+
+
+
+
+extension URLSession {
+    func data(
+        for request: URLRequest,
+        completion: @escaping(Result<Data, Error>) -> Void
+    ) -> URLSessionTask {
+        let fulfillCompletionOnTheMainThread: (Result<Data, Error>) -> Void = { result in
+            DispatchQueue.main.async {
+                completion(result)
+            }
+        }
+        let task = dataTask(with: request, completionHandler: { data, response, error in
+            if let data, let response, let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if 200..<300 ~= statusCode {
+                    fulfillCompletionOnTheMainThread(.success(data))
+                } else {
+                    fulfillCompletionOnTheMainThread(.failure(NetworkError.httpStatusCode(statusCode)))
+                }
+            } else if let error = error {
+                fulfillCompletionOnTheMainThread(.failure(NetworkError.urlRequestError(error))) // 5
+            } else {
+                fulfillCompletionOnTheMainThread(.failure(NetworkError.urlSessionError)) // 6
+            }
+        })
+        
+        return task
+    }
+}
