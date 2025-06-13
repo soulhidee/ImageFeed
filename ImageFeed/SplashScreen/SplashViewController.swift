096679baa85c36891cdcec35ae542158fcec0e49
soulhidee <serverpgig@yandex.ru>
2025-06-13 13:35:34 +0300

    feat: Implement SplashViewController logic and layout

--- ImageFeed/SplashScreen/SplashViewController.swift
+++ ImageFeed/SplashScreen/SplashViewController.swift
@@ -1,10 +1,38 @@
 import UIKit
 
-class SplashViewController: UIViewController {
+final class SplashViewController: UIViewController {
+    private let logoImageView = UIImageView()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+        
     }
     
+    private func setupViews() {
+        configureView()
+        configureLogoImageView()
+    }
     
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            logoImageView.widthAnchor.constraint(equalToConstant: 75),
+            logoImageView.heightAnchor.constraint(equalToConstant: 78),
+            logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    private func configureView() {
+        view.backgroundColor = UIColor(named: "YPBlack") ?? .black
+    }
+    
+    private func configureLogoImageView() {
+        logoImageView.image = UIImage(named: "LaunchScreenLogo")
+        logoImageView.contentMode = .scaleAspectFit
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoImageView)
+    }
 }
