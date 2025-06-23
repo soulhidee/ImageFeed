60b2cff2bf788746cd5a4df4eadebec790437f47
soulhidee <serverpgig@yandex.ru>
2025-06-23 20:04:50 +0300

    feat: Show blocking HUD after WebView closes

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -81,10 +81,10 @@ final class AuthViewController: UIViewController {
 
 extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
-        ProgressHUD.animate()
+        UIBlockingProgressHUD.show()
         oauth2Service.fetchAuthToken(code: code) { [weak self] result in
             guard let self else { return }
-            ProgressHUD.dismiss()
+            UIBlockingProgressHUD.dismiss()
             DispatchQueue.main.async {
                 switch result {
                 case .success(let token):
