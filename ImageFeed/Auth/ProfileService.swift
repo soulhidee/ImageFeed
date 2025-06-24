05954196989ab13b243115b11ea578a1b3fd3820
soulhidee <serverpgig@yandex.ru>
2025-06-24 12:17:36 +0300

    feat: Add ProfileResult model for user profile decoding

--- ImageFeed/Auth/ProfileService.swift
+++ ImageFeed/Auth/ProfileService.swift
@@ -1,1 +1,28 @@
 import Foundation
+
+final class ProfileService {
+    struct ProfileResult: Decodable {
+        let id: String
+        let username: String
+        let firstName: String?
+        let lastName: String?
+        let bio: String?
+        let email: String?
+        let profileImage: ProfileImage?
+        
+        enum CodingCase: String, CodingKey {
+            case id
+            case username
+            case firstName = "first_name"
+            case lastName = "last_name"
+            case bio
+            case profileImage = "profile_image"
+        }
+    }
+    
+    struct ProfileImage: Codable {
+        let small: String?
+        let medium: String?
+        let large: String?
+    }
+}
