83ac46f8f40200f5fbe1cd50249e24a9c9e7b865
soulhidee <serverpgig@yandex.ru>
2025-07-04 20:05:50 +0300

    refactor: Replace UserDefaults with SwiftKeychainWrapper for token storage

--- ImageFeed/Auth/AuthSupport/OAuth2TokenStorage.swift
+++ ImageFeed/Auth/AuthSupport/OAuth2TokenStorage.swift
@@ -1,15 +1,20 @@
 import UIKit
+import SwiftKeychainWrapper
 
 final class OAuth2TokenStorage {
     private let tokenKey = "UnsplashOAuthToken"
-    
-    var token: String? {
-        get {
-            UserDefaults.standard.string(forKey: tokenKey)
-        }
-        set {
-            UserDefaults.standard.set(newValue, forKey: tokenKey)
-        }
-    }
-}
+       
+       var token: String? {
+           get {
+               KeychainWrapper.standard.string(forKey: tokenKey)
+           }
+           set {
+               if let token = newValue {
+                   KeychainWrapper.standard.set(token, forKey: tokenKey)
+               } else {
+                   KeychainWrapper.standard.removeObject(forKey: tokenKey)
+               }
+           }
+       }
+   }
 
