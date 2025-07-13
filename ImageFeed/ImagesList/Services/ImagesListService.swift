abb2446a976ca56414c2faba26bd14bf6a71b961
soulhidee <serverpgig@yandex.ru>
2025-07-13 17:04:13 +0300

    feat(ImagesListService): add pagination and token storage properties

--- ImageFeed/ImagesList/Services/ImagesListService.swift
+++ ImageFeed/ImagesList/Services/ImagesListService.swift
@@ -1,12 +1,17 @@
 import Foundation
 
 final class ImagesListService {
-    private(set) var photos: [Photo] = []
-    
+    static let didChangeNotification = Notification.Name("ImagesListServiceDidChange")
+    private var task: URLSessionTask?
     private var lastLoadedPage: Int?
+    private(set) var photos: [Photo] = []
+    private let perPage = 10
+    private let session = URLSession.shared
+    private let tokenStorage = OAuth2TokenStorage()
+
     
     func fetchPhotosNextPage() {
-        
+    
     }
     
 }
