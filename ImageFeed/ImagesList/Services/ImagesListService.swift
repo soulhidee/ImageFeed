9f2106887af72bdd83f8b9ce44553330df1b654a
soulhidee <serverpgig@yandex.ru>
2025-07-13 17:08:26 +0300

    feat(ImagesListService): add URL builder method with pagination parameters

--- ImageFeed/ImagesList/Services/ImagesListService.swift
+++ ImageFeed/ImagesList/Services/ImagesListService.swift
@@ -8,10 +8,27 @@ final class ImagesListService {
     private let perPage = 10
     private let session = URLSession.shared
     private let tokenStorage = OAuth2TokenStorage()
-
+    
     
     func fetchPhotosNextPage() {
-    
+        guard task == nil else { return }
+        
+        guard let token = tokenStorage.token else {
+            print("[ImagesListService]: Токен отсутствует")
+            return
+        }
+        
+        let nextPage = (lastLoadedPage ?? .zero) + 1
+        
+        guard let url = makeURL(page: nextPage) else { return }
     }
     
+    private func makeURL(page: Int) -> URL? {
+        var components = URLComponents(string: "https://api.unsplash.com/photos")
+        components?.queryItems = [
+            URLQueryItem(name: "page", value: "\(page)"),
+            URLQueryItem(name: "per_page", value: "\(perPage)")
+        ]
+        return components?.url
+    }
 }
