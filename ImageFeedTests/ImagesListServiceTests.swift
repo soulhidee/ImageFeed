469f21b9aff3ad5a42d47531f1a1f48b7ed3c280
soulhidee <serverpgig@yandex.ru>
2025-07-13 17:55:38 +0300

    test

---
+++ ImageFeedTests/ImagesListServiceTests.swift
@@ -0,0 +1,23 @@
+import XCTest
+@testable import ImageFeed
+
+
+final class ImagesListServiceTests: XCTestCase {
+    func testFetchPhotos() {
+        let service = ImagesListService()
+        
+        let expectation = self.expectation(description: "Wait for Notification")
+        NotificationCenter.default.addObserver(
+            forName: ImagesListService.didChangeNotification,
+            object: nil,
+            queue: .main) { _ in
+                expectation.fulfill()
+            }
+        
+        service.fetchPhotosNextPage()
+        wait(for: [expectation], timeout: 10)
+        
+        XCTAssertEqual(service.photos.count, 10)
+    }
+}
+
