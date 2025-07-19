371a87ac7355118d3463f226844c5cc7e427a2cf
soulhidee <serverpgig@yandex.ru>
2025-07-19 18:26:01 +0300

    feat(ProfileLogoutService): Add logout method with cookie cleanup

--- ImageFeed/Profile/Services/ProfileLogoutService.swift
+++ ImageFeed/Profile/Services/ProfileLogoutService.swift
@@ -1,1 +1,21 @@
 import Foundation
+import WebKit
+
+final class ProfileLogoutService {
+    static let shared = ProfileLogoutService()
+    
+    private init() { }
+    
+    private func logout() {
+        cleanCookies()
+    }
+    
+    private func cleanCookies() {
+        HTTPCookieStorage.shared.removeCookies(since: Date.distantPast)
+        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+            records.forEach { record in
+                WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
+            }
+        }
+    }
+}
