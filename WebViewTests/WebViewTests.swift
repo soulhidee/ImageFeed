2c54cc272872f20f69418d63c37124a351f1723b
soulhidee <serverpgig@yandex.ru>
2025-08-03 12:21:48 +0300

    test: Add test to verify viewDidLoad call on presenter

--- WebViewTests/WebViewTests.swift
+++ WebViewTests/WebViewTests.swift
@@ -1,6 +1,35 @@
 @testable import ImageFeed
 import XCTest
 
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    var viewDidLoadCalled = false
+    weak var view: WebViewViewControllerProtocol?
+
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+    
+    func didUpdateProgressValue(_ newValue: Double) {
+    }
+
+    func code(from url: URL) -> String? {
+        return nil
+    }
+}
+
 final class WebViewTests: XCTestCase {
+    func testViewControllerCallsViewDidLoad() {
+        // given
+        let viewController = WebViewViewController()
+        let presenter = WebViewPresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+
+        // when
+        _ = viewController.view
+        
+        // then
+        XCTAssertTrue(presenter.viewDidLoadCalled, "viewDidLoad() у презентера должен быть вызван")
+    }
 }
 
