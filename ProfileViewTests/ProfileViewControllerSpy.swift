7323c7431afa5ab03acbc0e692709fa515f3dc22
soulhidee <serverpgig@yandex.ru>
2025-08-04 16:41:46 +0300

    test(ProfileViewController): Add ProfileViewControllerSpy for unit tests

--- ProfileViewTests/ProfileViewControllerSpy.swift
+++ ProfileViewTests/ProfileViewControllerSpy.swift
@@ -1,1 +1,21 @@
+@testable import ImageFeed
 import Foundation
+
+final class ProfileViewControllerSpy: ProfileViewControllerProtocol {
+    var presenter: ProfilePresenterProtocol?
+    var updateProfileDetailsCalled = false
+    var updateAvatarCalled = false
+    var stopShimmerAnimationCalled = false
+    
+    func updateProfileDetails(name: String, login: String, bio: String) {
+        updateProfileDetailsCalled = true
+    }
+    
+    func updateAvatar(with url: URL) {
+        updateAvatarCalled = true
+    }
+    
+    func stopShimmerAnimation() {
+        stopShimmerAnimationCalled = true
+    }
+}
