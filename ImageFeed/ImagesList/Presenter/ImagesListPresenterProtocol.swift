dc49e6faea331f147c61348023ae1ea1218fbbdf
soulhidee <serverpgig@yandex.ru>
2025-08-04 13:35:37 +0300

    feat: Add ImagesList MVP protocols

--- ImageFeed/ImagesList/Presenter/ImagesListPresenterProtocol.swift
+++ ImageFeed/ImagesList/Presenter/ImagesListPresenterProtocol.swift
@@ -1,1 +1,14 @@
 import Foundation
+import UIKit
+
+protocol ImagesListPresenterProtocol {
+    var view: ImagesListViewControllerProtocol? { get set }
+    
+    var photosCount: Int { get }
+    func viewDidLoad()
+    func photo(at index: Int) -> Photo
+    func sizeForPhoto(at index: Int) -> CGSize
+    func configCell(_ cell: ImagesListCell, for index: Int)
+    func didSelectImage(at index: Int)
+    func didTapLike(at index: Int)
+}
--- ImageFeed/ImagesList/View/ImagesListViewControllerProtocol.swift
+++ ImageFeed/ImagesList/View/ImagesListViewControllerProtocol.swift
@@ -1,5 +1,9 @@
 import Foundation
 
 protocol ImagesListViewControllerProtocol: AnyObject {
-
+    var presenter: ImagesListPresenterProtocol? { get set }
+    
+    func updateTableViewAnimated()
+    func reloadCell(at indexPath: IndexPath)
+    func showLikeError(message: String)
 }
