234adcde334dede36404183444abd24657962f8a
soulhidee <serverpgig@yandex.ru>
2025-08-04 13:24:12 +0300

    chore: Add helper folders to project

--- ImageFeed/ImagesList/ImagesListViewControllerProtocol.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-protocol ImagesListViewControllerProtocol: AnyObject {
-    func updateTableView(with indexPaths: [IndexPath])
-    func configureCell(at indexPath: IndexPath, with photo: Photo, dateText: String, isLiked: Bool)
-    func showSingleImage(with url: URL)
-    func showAuthErrorAlert()
-}
