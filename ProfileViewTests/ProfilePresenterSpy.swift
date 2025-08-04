8961338a0a4f704a07eef8813f73cdc289dda9b2
soulhidee <serverpgig@yandex.ru>
2025-08-04 16:40:33 +0300

    test(ProfileViewController): Add ProfilePresenterSpy for unit tests

--- ProfileViewTests/ProfilePresenterSpy.swift
+++ ProfileViewTests/ProfilePresenterSpy.swift
@@ -2,5 +2,20 @@
 import Foundation
 
 final class ProfilePresenterSpy: ProfilePresenterProtocol {
+    weak var view: ProfileViewControllerProtocol?
+    var viewDidLoadCalled = false
+    var didTapLogoutCalled = false
+    var avatarDidChangeCalled = false
     
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+    
+    func didTapLogout() {
+        didTapLogoutCalled = true
+    }
+    
+    func avatarDidChange() {
+        avatarDidChangeCalled = true
+    }
 }
