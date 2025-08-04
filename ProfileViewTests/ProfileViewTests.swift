cf164817f7e5987f0e02dd07c215c0571342631c
soulhidee <serverpgig@yandex.ru>
2025-08-04 16:45:17 +0300

    test(ProfileViewController): Add unit test for viewDidLoad call

--- ProfileViewTests/ProfileViewTests.swift
+++ ProfileViewTests/ProfileViewTests.swift
@@ -2,6 +2,20 @@
 import XCTest
 
 final class ProfileViewControllerTests: XCTestCase {
+    
+    func testViewControllerCallsViewDidLoad() {
+        // given
+        let viewController = ProfileViewController()
+        let presenter = ProfilePresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        // when
+        _ = viewController.view
+        
+        // then
+        XCTAssertTrue(presenter.viewDidLoadCalled, "viewDidLoad() у презентера должен быть вызван")
+    }
 }
 
 
