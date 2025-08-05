3df9175ab8d5d497e1424339524009fac9546cd2
soulhidee <serverpgig@yandex.ru>
2025-08-05 16:38:46 +0300

    test(ImagesListViewTests): Add test for inserting new rows on photos update

--- ImagesListViewTests/ImagesListViewTests.swift
+++ ImagesListViewTests/ImagesListViewTests.swift
@@ -14,16 +14,31 @@ final class ImagesListViewTests: XCTestCase {
     }
     
     func testViewControllerCallsViewDidLoad() {
-            // given
-            let viewController = ImagesListViewController()
-            let presenter = ImagesListPresenterSpy()
-            viewController.presenter = presenter
-            presenter.view = viewController
-            
-            // when
-            _ = viewController.view
-            
-            // then
-            XCTAssertTrue(presenter.viewDidLoadCalled, "viewDidLoad() у презентера должен быть вызван")
-        }
+        // given
+        let viewController = ImagesListViewController()
+        let presenter = ImagesListPresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        // when
+        _ = viewController.view
+        
+        // then
+        XCTAssertTrue(presenter.viewDidLoadCalled, "viewDidLoad() у презентера должен быть вызван")
+    }
+    
+    func testPresenterInsertsNewRowsOnPhotosUpdate() {
+        // given
+        let service = ImagesListServiceMock()
+        let presenter = ImagesListPresenter(imagesListService: service)
+        let view = ImagesListViewControllerSpy()
+        presenter.view = view
+        
+        // when
+        service.fetchPhotosNextPage()
+        NotificationCenter.default.post(name: ImagesListService.didChangeNotification, object: nil)
+        
+        // then
+        XCTAssertTrue(view.insertNewRowsCalled, "insertNewRows должен быть вызван при обновлении списка фотографий")
+    }
 }
