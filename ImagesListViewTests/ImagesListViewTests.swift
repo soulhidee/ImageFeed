a297335919b744ee6dab2668ba20d2a324c2cb65
soulhidee <serverpgig@yandex.ru>
2025-08-05 16:33:32 +0300

    test(ImagesListViewTests): Add base test setup for ImagesListView

--- ImagesListViewTests/ImagesListViewTests.swift
+++ ImagesListViewTests/ImagesListViewTests.swift
@@ -1,2 +1,15 @@
 @testable import ImageFeed
 import XCTest
+
+final class ImagesListViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        ImagesListService.shared.reset()
+    }
+    
+    override func tearDown() {
+        NotificationCenter.default.removeObserver(ImagesListPresenter.self)
+        super.tearDown()
+    }
+}
